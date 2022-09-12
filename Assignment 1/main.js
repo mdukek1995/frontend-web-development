4b78f408a8c16407ef885886ae59813fdabb2a90
mdukek1995 <89478873+mdukek1995@users.noreply.github.com>
2022-09-12 14:08:39 -0600

    Assignment 1

---
+++ Assignment 1/main.js
@@ -0,0 +1,35 @@
+const sunImg = document.getElementById("sun");
+
+function changeToNight() {
+    document.getElementById('sun').style.display = 'none';
+    document.body.style.backgroundColor = "#000000" ;
+    document.getElementById('moon').hidden = false;
+    document.getElementById('moon').style.display = 'block';
+};
+
+sunImg.addEventListener('click', () => {
+    changeToNight();
+});
+
+const moonImg = document.getElementById("moon");
+
+function changeToDay() {
+    document.getElementById('moon').style.display = 'none';
+    document.body.style.backgroundColor = "#BFD7EA" ;
+    sunImg.style.display = 'block' ;
+};
+
+moonImg.addEventListener('click', () => {
+    changeToDay();
+});
+
+function addClouds() {
+    var img = document.createElement("img");
+    img.src = "clouds.png";
+    var src = document.getElementById("day-night");
+    src.appendChild(img);
+}
+
+window.addEventListener('keyup', () => {
+    addClouds();
+})
