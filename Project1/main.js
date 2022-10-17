cc3e4a3ccb3e951cfd46b6758d03fd94781c61c6
mdukek1995 <89478873+mdukek1995@users.noreply.github.com>
2022-10-17 13:49:05 -0600

    Added blubber, bubbles, sun, and text

--- Project1/main.js
+++ Project1/main.js
@@ -31,15 +31,52 @@ zoom.addEventListener('wheel', (e) => {
 
 // after zoom, maybe I can append the image and set the criteria? That way it won't be showing
 
-console.log()
+
 let maleOrca = document.getElementById('male-orca');
 let femaleOrca = document.getElementById('female-orca');
 let fish = document.getElementById('salmon');
+let cloudOne = document.getElementById('cloud-one');
+let cloudTwo = document.getElementById('cloud-two');
+let cloudThree = document.getElementById('cloud-three');
+let sun = document.getElementById('sun');
+let h1 = document.querySelector('h1');
+let h2Title = document.getElementById('title');
+let bubbles = document.getElementById('bubbles');
+let blubber = document.getElementById('blubber');
 
-console.log()
+const whales = document.getElementById('whales');
+const fishDiv = document.getElementById('fish');
+
 window.addEventListener('scroll', (e) => {
-  let value = window.scrollY;
-  console.log(value); 
-  femaleOrca.style.left = -value*0.25 + 'px';
-  maleOrca.style.left = -value*0.17 + 'px';
-})
+  let totalScroll = window.scrollY;
+  let whalePosition = whales.offsetTop;
+  let fishPosition = fishDiv.offsetTop;
+  let distanceWhale = totalScroll-whalePosition;
+  let distanceSalmon = totalScroll-fishPosition;
+
+  let femaleOrcaX = 500 + (-distanceWhale*.6);
+  let maleOrcaX = 500 + (-distanceWhale*.4);
+  let cloudOneY = 100-(totalScroll*0.5);
+  let cloudTwoY = 150-(totalScroll*0.7);
+  let cloudThreeY = 10-(totalScroll*2);
+  let cloudOneX = 0+(totalScroll*0.8);
+  let cloudTwoX = 500+(totalScroll*1);
+  let cloudThreeX = 700+(totalScroll*0.5);
+  let sunY = 20+(totalScroll*1.3);
+  let titleOpacity = 1 - (totalScroll*0.004);
+  let bubbleY = 0 - (totalScroll*0.2);
+  let blubberOpacity = 0 + (distanceSalmon*0.00065);
+
+  femaleOrca.style.transform = `translateX(${femaleOrcaX}px)`;
+  maleOrca.style.transform = `translateX(${maleOrcaX}px)`;
+  cloudOne.style.transform = `translate(${cloudOneX}px, ${cloudOneY}px)`;
+  cloudTwo.style.transform = `translate(${cloudTwoX}px, ${cloudTwoY}px)`;
+  cloudThree.style.transform = `translate(${cloudThreeX}px,${cloudThreeY}px)`; 
+  sun.style.transform = `translate(0px, ${sunY}px)`;
+  h1.style.top = totalScroll*1.3 + 'px';
+  h1.style.opacity = titleOpacity;
+  h2Title.style.top = totalScroll*1.3 + 'px';
+  h2Title.style.opacity = titleOpacity;
+  bubbles.style.top = bubbleY + 'px';  
+  blubber.style.opacity = blubberOpacity;
+});
